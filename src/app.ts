288c1fec8a0130ac0cc12ccbf57796970b0792b4
Karim <karim.maged2020@yahoo.com>
2025-03-31 18:37:18 +0200

    Add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./db";
+import errorHandler from "./error";
 
 // Initialize .env
 dotenv.config();
@@ -13,8 +14,12 @@ app.use(express.json());
 // connect to database
 connectDb();
 
+// use global error handler
+app.use(errorHandler);
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () =>
+  // eslint-disable-next-line
   console.log(`Server running on http://localhost:${PORT}`)
 );
